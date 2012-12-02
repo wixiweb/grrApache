ffcd3c369912ebcbb62d5b4f7136308bfb88ead5
user <user@wixidell>
2012-12-02 04:11:44 +0100

    Merge upstream

--- src/app/grrapache.js
+++ src/app/grrapache.js
@@ -1,6 +1,62 @@
 var grrapache = grrapache || {};
+/**
+ * 
+ */
+grrapache.controller = {
+    /**
+     * 
+     */
+    getServerList : function() {
+        var collection = [],
+            service = new grrapache.service.server(
+            grrapache.model.parserInfo,
+            grrapache.model.parserStatus
+        );
 
-$(function() {
+        var server = service.getServer(
+            'localhost',
+            'http://localhost:8080'
+        );
+            
+        var serverToItem = function (model) {
+            var flags = [
+                'waiting',
+                'starting',
+                'reading',
+                'sending',
+                'keepalive',
+                'dns',
+                'closing',
+                'logging',
+                'finishing',
+                'cleaning',
+                'idle'
+            ],
+                status = {}
+            ;
+            
+            for (var i = 0; i < flags.length; i++) {
+                status[flags[i]] = model.getScoreboard().countByKey(flags[i]);
+            } 
+            
+            return {
+                'title':model.getLabel(),
+                'website':model.getUrl(),
+                'maxClient':model.getActualMaxClient(),
+                'status':status,
+                'indice':Math.round(
+                    parseFloat(model.getStatusData().cpuload) * 100
+                ) / 100 + '%'
+            };
+        }
+        
+        collection.push(serverToItem(server));
+        
+        return collection;
+    }
+}
+
+/*$(function() {
 
     var service = new grrapache.service.server(
         grrapache.model.parserInfo,
@@ -11,5 +67,5 @@ $(function() {
 
     console.log(server);
 
-});
+});*/
 
